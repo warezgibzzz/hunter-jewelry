52fe233a91da095b36633f21b6c2879d9934d6a8
Pavel Poronko <me@gibzzz.tk>
2017-03-13 02:38:53 +0700

    Front - nested active class in menu

--- src/Hunter/FrontendBundle/Menu/Builder.php
+++ src/Hunter/FrontendBundle/Menu/Builder.php
@@ -24,8 +24,30 @@ class Builder
     {
         $menu = $this->factory->createItem('root');
 
-        $menu->addChild('frontend.menu.nav.shop', ['route' => 'hunter_frontend_shop_index']);
-        $menu->addChild('frontend.menu.nav.blog', ['route' => 'hunter_frontend_blog_index']);
+        $menu->addChild(
+            'frontend.menu.nav.shop',
+            [
+                'route' => 'hunter_frontend_shop_index',
+                'extras' => [
+                    'routes' => [
+                        ['route' => 'hunter_frontend_shop_category'],
+                        ['route' => 'hunter_frontend_shop_product'],
+                    ]
+                ]
+            ]
+        );
+        $menu->addChild(
+            'frontend.menu.nav.blog',
+            [
+                'route' => 'hunter_frontend_blog_index',
+                'extras' => [
+                    'routes' => [
+                        ['route' => 'hunter_frontend_blog_category'],
+                        ['route' => 'hunter_frontend_blog_post'],
+                    ]
+                ]
+            ]
+        );
         $menu->addChild('frontend.menu.nav.about', ['route' => 'hunter_frontend_about']);
 
         return $menu;
